36380dafff26443802e8f5843c60e4837222aaf4
Marcelo Aguiar <mclaguiar@hotmail.com>
2017-11-17 16:41:30 -0200

    projeto inicial

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Reflection;
 
 namespace ReadAssemblyVersion
 {
     class Program
     {
-        static void Main(string[] args)
+        private enum ExitCode
         {
-            Console.WriteLine("Hello World!");
+            Success = 0,
+            InvalidUsage = -1
         }
+
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Write("Invalid Arguments");
+                return (int)ExitCode.InvalidUsage;
+            }
+
+            try
+            {
+                if (System.IO.File.Exists(args[0]))
+                {
+                    var filePath = args[0];
+                    Assembly assembly = Assembly.LoadFrom(filePath);
+                    Version version = assembly.GetName().Version;
+                    Console.Write(version.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write($"Error : {ex.Message}");
+                return (int)ExitCode.InvalidUsage;
+            }
+            return (int)ExitCode.Success;
+        }
+
     }
 }
